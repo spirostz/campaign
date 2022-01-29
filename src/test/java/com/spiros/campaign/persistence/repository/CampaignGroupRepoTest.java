f77b4000853f3ac731ab4ae296c459ca439a13f5
spirostz <tzoras.spiros@gmail.com>
2022-01-29 11:47:14 +0200

    wip testing

--- src/test/java/com/spiros/campaign/persistence/repository/CampaignGroupRepoTest.java
+++ src/test/java/com/spiros/campaign/persistence/repository/CampaignGroupRepoTest.java
@@ -1,8 +1,17 @@
 package com.spiros.campaign.persistence.repository;
 
+import com.spiros.campaign.common.enums.OptimisationStatusType;
+import com.spiros.campaign.persistence.entity.CampaignEntity;
+import com.spiros.campaign.persistence.entity.CampaignGroupEntity;
+import com.spiros.campaign.persistence.entity.OptimisationEntity;
+import com.spiros.campaign.persistence.entity.RecommendationEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,9 +21,36 @@ class CampaignGroupRepoTest {
     @Autowired
     private CampaignGroupRepo campaignGroupRepo;
 
-
     @Test
-    void name() {
+    void testSampleCampaignGroup() {
+
+        CampaignGroupEntity campaignGroup = new CampaignGroupEntity();
+        campaignGroup.setName("campaignGroupTest");
+
+        CampaignEntity campaign1 = new CampaignEntity();
+        campaign1.setCampaignGroup(campaignGroup);
+
+        CampaignEntity campaign2 = new CampaignEntity();
+        campaign2.setCampaignGroup(campaignGroup);
+
+        OptimisationEntity optimisation = new OptimisationEntity();
+        optimisation.setCampaignGroup(campaignGroup);
+        optimisation.setOptimisationStatus(OptimisationStatusType.NOT_APPLIED);
+
+        RecommendationEntity recommendation1 = new RecommendationEntity();
+        recommendation1.setCampaign(campaign1);
+        recommendation1.setOptimisation(optimisation);
+        campaign1.setRecommendation(recommendation1);
+
+        RecommendationEntity recommendation2 = new RecommendationEntity();
+        recommendation2.setCampaign(campaign2);
+        recommendation2.setOptimisation(optimisation);
+        campaign2.setRecommendation(recommendation2);
+
+        campaignGroup.setCampaignEntities(new HashSet<>(Arrays.asList(campaign1, campaign2)));
+
+        CampaignGroupEntity campaignGroupSaved = campaignGroupRepo.save(campaignGroup);
+
         assertTrue(true);
     }
 }
